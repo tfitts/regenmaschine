ea05d8c8131364ca86e2242925f2442a56bd5dc8
Aaron Bach <bachya1208@gmail.com>
2017-08-13 17:22:24 -0600

    Added ability to specify local port

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,7 +12,6 @@ Github: https://github.com/bachya/regenmaschine
 import json
 
 import pytest
-import requests
 import requests_mock
 
 import regenmaschine as rm
@@ -95,6 +94,20 @@ class TestLocalAuthenticator(object):
             with pytest.raises(rm.exceptions.HTTPError) as exc_info:
                 rm.Authenticator.create_local('192.168.1.100', '12345')
                 assert '404 Client Error: Not Found' in str(exc_info)
+
+    def test_local_different_port(self, local_cookies,
+                                  local_auth_response_200):
+        """ Tests setting a different port for local connections """
+        url = 'https://192.168.1.100:80/api/4/auth/login'
+        with requests_mock.Mocker() as mock:
+            mock.post(
+                url,
+                text=json.dumps(local_auth_response_200),
+                cookies=local_cookies)
+
+            auth = rm.Authenticator.create_local(
+                '192.168.1.100', '12345', port=80)
+            assert ':80/' in auth.url
 
 
 class TestRemoteCredentials(object):
